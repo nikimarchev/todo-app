d21ef5b819896436284589df5e92465d8406439a
Nikola Marchev <nikiskate99@gmail.com>
2021-03-04 16:44:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([]);
+  const [marked, setMarked] = useState(false);
 
-    </div>
+  const addItems = () => {
+    if (input.length > 0) {
+      setList(oldItems => [...oldItems, input]);
+      setInput('');
+    }
+  }
+
+  const removeItem = (item) => {
+    setList(items => items.filter(x => x !== item))
+  }
+
+  return (
+    <div className="app">
+      <div className="header">TODO LIST</div>
+      <div className="body">
+        <input className="input" type="text" value={input} onChange={e => { setInput(e.target.value) }}></input>
+        <button className="addButton" onClick={addItems}>ADD</button>
+      </div>
+      <div className="counter">{list.length} TODOS</div>
+      <ul className="container">
+        {
+          list.map(item => (
+            <li className={marked ? "executedItem" : "item"}>
+              {item}
+              <button className="editButton" onClick={() => setMarked(!marked)}>Executed</button>
+              <button className="deleteButton" onClick={() => { removeItem(item) }}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>
+    </div >
   );
 }
 
